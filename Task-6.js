446102c6cb7a6fb6ce8df0b3a96f2637606ae6c3
Sneha harigharun <109866539+sneha2001@users.noreply.github.com>
2023-11-29 12:10:27 +0530

    Update Task-6.js

--- Task-6.js
+++ Task-6.js
@@ -28,6 +28,31 @@ var movie2 = new Movie("The Dark Knight", "Warner Bros", "PG-13");
 console.log(movie2.title); // Output: The Dark Knight
 console.log(movie2.studio); // Output: Warner Bros
 console.log(movie2.rating); // Output: PG-13
+//c)
+class Movie {
+  constructor(title, studio, rating) {
+    this.title = title;
+    this.studio = studio;
+    this.rating = rating;
+  }
+}
+function getPG(movies) {
+  // Filter movies with a rating of "PG"
+  const pgMovies = movies.filter(movie => movie.rating === "PG");
+  
+  return pgMovies;
+}
+// Example usage:
+const movies = [
+  new Movie("Movie1", "Studio1", "PG"),
+  new Movie("Movie2", "Studio2", "R"),
+  new Movie("Movie3", "Studio3", "PG"),
+  new Movie("Movie4", "Studio4", "PG-13"),
+];
+
+const pgMovies = getPG(movies);
+console.log(pgMovies);
+
 
 
 
